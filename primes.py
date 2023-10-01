155962bb574a56bc45a0811e991e03f096e4ee24
Alex Tsybulnikov <alex.tsybulnikov@gmail.com>
2023-10-01 22:46:12 +0100

    list of prime numbers generator

--- primes.py
+++ primes.py
@@ -2,5 +2,21 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
+
     list = []
+    num = 2
+    while len(list) < number_of_primes:
+        if isPrime(num):
+            list.append(num)
+        num += 1
     return list
+
+def isPrime(n):
+    if n == 2:
+        return True
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
